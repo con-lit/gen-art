128b8d34b4baa9c07683adf9e121bee7dc66fe52
Constantin Litvak <litvak@d-sire.com>
2025-01-03 17:17:39 +0100

    setup fixed

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ python manage.py create_superuser
 # Check if the environment is production
 if [ "$DJANGO_ENV" = "production" ]; then
     # Start Hypercorn in production
-    hypercorn --bind 0.0.0.0:8000 app.asgi:application
+    hypercorn --bind 0.0.0.0:8000 consta.asgi:application
 else
     # Start the Django development server on local machine
     python manage.py runserver 0.0.0.0:8000
